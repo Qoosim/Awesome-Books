1a4a23d4237f88538a1ddef599f5825acbadf9a8
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-10-21 02:02:12 +0100

    Add style to the awesome book page

--- script.js
+++ script.js
@@ -8,15 +8,22 @@ const output = document.querySelector('.output');
 
 const createBook = ({ title, author, id }) => {
   const div = document.createElement('div');
+  div.classList.add(
+    'row',
+    'm-1',
+    'justify-content-center',
+    'custom-row',
+  );
 
   div.innerHTML = `
-    <h3>${title}</h3>
-    <h3>${author}</h3>
-    <button class="delete btnDelete p-1 rounded" id="${id}" onclick="library.removeBook(this)">Remove</button>
-    <hr>
+    <h4 class="col-sm-10">"${title}" by ${author}</h4>
+    <button class="col-sm-2 btn-danger rounded" id="${id}" onclick="library.removeBook(this)">Remove</button>
     `;
   output.appendChild(div);
+  output.style.display = library.books.length === 0 ? 'none' : 'block';
 };
+
+output.style.display = library.books.length === 0 ? 'none' : 'block';
 
 // Add event listener to the add button
 btnAdd.addEventListener('click', (event) => {
